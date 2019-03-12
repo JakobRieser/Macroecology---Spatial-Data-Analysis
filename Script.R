16fec467284f96f9cb08c127f000a6e5622dc8bf
malinfischer <malin.fischer@web.de>
2019-03-12 15:27:06 +0100

    test2

--- Script.R
+++ Script.R
@@ -48,8 +48,8 @@ library(ggplot2)
 getwd()
 
 #set the directory to the folder you want to work in and get & store your data:
-setwd("D:/Programme/OneDrive/EAGLE M.Sc/Term 1 (Winter 2018 - 2019)/Macroecology/Data/") 
-#setwd("C:/Users/Malin/Documents/Studium/Wuerzburg/Macroecology_Conference/NDWI_data") 
+#setwd("D:/Programme/OneDrive/EAGLE M.Sc/Term 1 (Winter 2018 - 2019)/Macroecology/Data/") 
+setwd("C:/Users/Malin/Documents/Studium/Wuerzburg/Macroecology_Conference/Macroecology-Spatial-Data-Analysis/Data") 
 
 #this is important, when you don't want to change your directory for every dataset you want to load in your R workspace
 
